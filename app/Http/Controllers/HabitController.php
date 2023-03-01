d887588897e6f01d92e28fdaf659898e3c9f010e
adamxzz <adamzaman51@gmail.com>
2023-03-01 15:28:45 +0000

    first commit

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\HabitCollection;
+use App\Http\Resources\HabitResource;
+use App\Models\Habit;
+use Illuminate\Http\Request;
+
+class HabitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new HabitCollection(Habit::all());   
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Habit $habit)
+    {
+        //
+    }
+}
